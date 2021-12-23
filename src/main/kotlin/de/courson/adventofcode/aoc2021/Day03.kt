d0c583ff380d48e56743bcb937d3ee14cfae84fe
Dennis Bayer <Dennis.Bayer.82@gmail.com>
2021-12-23 11:08:37 +0100

    Solve AoC 2021, Day 03 - Part 2

--- src/main/kotlin/de/courson/adventofcode/aoc2021/Day03.kt
+++ src/main/kotlin/de/courson/adventofcode/aoc2021/Day03.kt
@@ -3,6 +3,7 @@ package de.courson.adventofcode.aoc2021
 import readInput
 
 fun main() {
+
 	fun List<String>.countBitPair(index: Int): Pair<Int, Int> {
 		var zeros = 0
 		var ones = 0
@@ -37,15 +38,40 @@ fun main() {
 		return gamma.toInt(2) * epsilon.toInt(2)
 	}
 
-//	fun part2(input: List<String>): Int {
-//		return input.size
-//	}
+	fun part2(input: List<String>): Int {
+		val measurementLength = input.first().length
+
+		var oxygen = input
+		for (i in 0 until measurementLength) {
+			if (oxygen.size == 1) break
+
+			val columnBits = oxygen.countBitPair(i)
+			val keepZeros = columnBits.first > columnBits.second
+			oxygen = oxygen.filter { measurement ->
+				if (keepZeros) measurement[i] == '0' else measurement[i] == '1'
+			}
+		}
+
+		var co2 = input
+		for (i in 0 until measurementLength) {
+			if (co2.size == 1) break
+
+			val columnBits = co2.countBitPair(i)
+			val keepZeros = columnBits.first <= columnBits.second
+			co2 = co2.filter { measurement ->
+				if (keepZeros) measurement[i] == '0' else measurement[i] == '1'
+			}
+		}
+
+		return oxygen.first().toInt(2) * co2.first().toInt(2)
+	}
 
 	// test if implementation meets criteria from the description, like:
 	val testInput = readInput("Day03_test")
 	check(part1(testInput) == 198)
+	check(part2(testInput) == 230)
 
 	val input = readInput("Day03")
 	println(part1(input))
-//	println(part2(input))
+	println(part2(input))
 }
